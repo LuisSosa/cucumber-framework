d12d93312a4d2160e5f3da84540c5877c63e5da6
LuisSosa <ing.lasosa@gmail.com>
2022-02-07 11:19:56 -0800

    updating sh scripts

--- features/files.sh
+++ features/files.sh
@@ -1,25 +1,7 @@
 #! /bin/sh
 
-now=$(date +"%H-%M-%S")
-new_folder=$(date '+%Y-%m-%d')
-new_subFolder="features/reports/$new_folder/$now"
-new_folder_sc="features/screenshots/$new_folder"
-new_subFolder_sc="features/screenshots/$new_folder/$now"
-
-#Create Subfolder to move all reports created by date and time
-if [ -d "features/reports/$new_folder" ]
+#Check if exists path and remove all files generated for allure report
+if [ -d "features/reports/temp" ]
 then
-    #Directory exists
-    mkdir "$new_subFolder"
-    sleep 2
-    find "features/reports/temp" -type f \( -iname \*.json -o -iname \*.png \) -exec mv {} "$new_subFolder" \;
-else
-    #Directory does not exist
-    mkdir "features/reports/$new_folder"
-    sleep 2
-    mkdir "$new_subFolder"
-    sleep 2
-    find "features/reports/temp" -type f \( -iname \*.json -o -iname \*.png \) -exec mv {} "$new_subFolder" \;
+    rm -rf features/reports/temp/*
 fi
-
-#allure serve "$new_subFolder"
--- features/suite.sh
+++ features/suite.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
+
+sh "features/files.sh"
+
 behave -f allure_behave.formatter:AllureFormatter -o features/reports/temp \
 features/search.feature \
 features/add_product_to_bag.feature \
 features/remove_from_bag.feature
-
-sh "features/files.sh"
